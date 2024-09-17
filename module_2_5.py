bc7d9accaf90fcdc8341fb207b5889a9264b4557
OlgaAL525 <mouse5525@gmail.com>
2024-09-18 02:41:35 +0300

    Add files via upload

--- module_2_5.py
+++ module_2_5.py
@@ -1,18 +1,29 @@
+
+
+
 def  get_matrix(n, m, value):
     matrix = []
-    for i in range(int(n)):
+    for i in range(n):
         matrix_s = []
-        for j in range(int(m)):
+        for j in range(m):
             matrix_s.append(value)
         matrix.append(matrix_s)
-
     return matrix
 
-
-result1 = get_matrix(2, 2, 10)
+n = int(input('Количество строк:'))
+if n <= 0:
+    while n <= 0:
+        print ('количество строк не может быть отрицательным или равным нулю')
+        n = int(input('Попробуйте еще раз:'))
+m = int(input('Количество столбцов:'))
+if m <= 0:
+    while m <= 0:
+        print ('количество столбцов не может быть отрицательным или равным нулю')
+        m = int(input('Попробуйте еще раз'))
+value = int(input('Значение элемента матрицы:'))
+result1 = get_matrix(n, m, value)
 print(result1)
-result2 = get_matrix(3, 2, 5)                     
-print(result2)
+
 
 
 
