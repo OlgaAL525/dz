070b7e5e73b04dcf07f14aebb924fcb1d770c8e5
OlgaAL525 <mouse5525@gmail.com>
2024-09-29 21:55:17 +0300

    Add files via upload

---
+++ module_3_5.py
@@ -0,0 +1,10 @@
+def get_multiplied_digits(namber):
+    str_namber = str(namber)
+    first = int(str_namber[0])
+    if len(str_namber) == 1:
+        return first
+    else:
+        return first * get_multiplied_digits(int(str_namber[1:]))
+
+result = get_multiplied_digits(809703)
+print (result)
